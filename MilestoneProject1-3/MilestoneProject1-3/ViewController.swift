5298aff20e98019a38b6fd59f06aae7697fd2b04
Davy <dchuon@ucdavis.edu>
2023-02-13 16:05:11 -0800

    created initial uitableview screen and loaded the flags into the cells

---
+++ MilestoneProject1-3/MilestoneProject1-3/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  MilestoneProject1-3
+//
+//  Created by Davy Chuon on 2/13/23.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var origFlagsList = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasSuffix("2x.png") {
+                origFlagsList.append(item)
+            }
+        }
+
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Flag", for: indexPath)
+        
+        cell.imageView?.image = UIImage(named: origFlagsList[indexPath.row])
+        cell.imageView?.layer.borderWidth = 0.5
+        cell.imageView?.layer.borderColor = UIColor.black.cgColor
+        
+        cell.textLabel?.text = String(origFlagsList[indexPath.row].split(separator: "@").first!).uppercased()
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return origFlagsList.count
+    }
+    
+
+
+}
+
