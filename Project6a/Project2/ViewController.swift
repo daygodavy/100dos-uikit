0a1c11aed48dbc40cd2698428f3b7a4f9256eb40
Davy <dchuon@ucdavis.edu>
2023-02-24 16:25:06 -0800

    Project 6a: refactoring Project 2 to accommodate landscape mode without issues

---
+++ Project6a/Project2/ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Davy Chuon on 2/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    var correctAnswer = 0
+    var questionsAsked = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        countries += ["estonia", "france", "germany",
+                      "ireland", "italy", "monaco", "nigeria",
+                      "poland", "russia", "spain", "uk", "usa"]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        button1.configuration?.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+        button2.configuration?.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+        button3.configuration?.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+        
+        askQuestion()
+    }
+    
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
+        button1.setImage(UIImage(named: countries[0])?.withRenderingMode(.alwaysOriginal), for: .normal)
+        button2.setImage(UIImage(named: countries[1])?.withRenderingMode(.alwaysOriginal), for: .normal)
+        button3.setImage(UIImage(named: countries[2])?.withRenderingMode(.alwaysOriginal), for: .normal)
+        
+        title = countries[correctAnswer].uppercased()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Score: \(score)", style: .plain, target: self, action: nil)
+        
+        navigationItem.rightBarButtonItem?.isEnabled = false
+        
+    }
+
+    @IBAction func flagButtonTapped(_ sender: UIButton) {
+        var title: String
+        
+        questionsAsked += 1
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong, that flag is \(countries[sender.tag].uppercased())"
+            score -= 1
+        }
+        
+        if questionsAsked == 10 {
+            let ac = UIAlertController(title: title, message: "10 Questions Completed: Your score is \(score)", preferredStyle: .alert)
+            
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            
+            present(ac, animated: true)
+            questionsAsked = 0
+        } else {
+            let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+            
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            
+            present(ac, animated: true)
+        }
+        
+    }
+    
+}
+
