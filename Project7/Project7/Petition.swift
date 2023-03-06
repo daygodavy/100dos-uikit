b8a5272f6fe98b421bb4e58e9ffcad00b1640c63
Davy <dchuon@ucdavis.edu>
2023-03-06 13:04:21 -0800

    added navcontroller and tabbarcontroller, created Petition and Petitions structs, created logic and function to parse json data and load into tableview cells

---
+++ Project7/Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Davy Chuon on 3/6/23.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
