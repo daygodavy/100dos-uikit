11615479fd53f7f3849dc3f2ebd5eecc544de4d3
Davy <dchuon@ucdavis.edu>
2023-04-11 13:53:03 -0700

    starting Project 12: UserDefaults (duplicates of Proj 10)

---
+++ Project12a/Project10/ViewController.swift
@@ -0,0 +1,98 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Davy Chuon on 3/30/23.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    var people = [Person]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
+    }
+    
+    @objc func addNewPerson() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+            try? jpegData.write(to: imagePath)
+        }
+        let person = Person(name: "Unknown", image: imageName)
+        people.append(person)
+        collectionView.reloadData()
+        
+        
+        dismiss(animated: true)
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return people.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue PersonCell")
+        }
+        
+        let person = people[indexPath.item]
+        cell.name.text = person.name
+        let path = getDocumentsDirectory().appendingPathComponent(person.image)
+        cell.imageView.image = UIImage(contentsOfFile: path.path)
+        cell.imageView.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
+        cell.imageView.layer.borderWidth = 2
+        cell.imageView.layer.cornerRadius = 3
+        cell.layer.cornerRadius = 7
+        
+        return cell
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let person = people[indexPath.item]
+        
+        let initAC = UIAlertController(title: "Selected \(person.name)", message: "Would you like to rename or delete this person?", preferredStyle: .alert)
+        let action1 = UIAlertAction(title: "Rename", style: .default) { [weak self] _ in
+            let ac1 = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
+            ac1.addTextField()
+            
+            ac1.addAction(UIAlertAction(title: "OK", style: .default) { [weak self, weak ac1] _ in
+                guard let newName = ac1?.textFields?[0].text else { return }
+                person.name = newName
+                self?.collectionView.reloadData()
+            })
+            
+            ac1.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+            self?.present(ac1, animated: true)
+        }
+        
+        let action2 = UIAlertAction(title: "Delete", style: .destructive) { [weak self] _ in
+            self?.people.remove(at: indexPath.item)
+            self?.collectionView.reloadData()
+        }
+        
+        let action3 = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        initAC.addAction(action1)
+        initAC.addAction(action2)
+        initAC.addAction(action3)
+        present(initAC, animated: true)
+    }
+
+}
+
---
+++ Project12b/Project10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Davy Chuon on 3/30/23.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+    
+}
