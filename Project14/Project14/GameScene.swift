a33c5affb10da805789a52362741714d7ae754f0
Davy <dchuon@ucdavis.edu>
2023-05-02 16:30:30 -0700

    Starting Project 14 - Whack A Penguin: setup project and added assets

---
+++ Project14/Project14/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  Project14
+//
+//  Created by Davy Chuon on 5/2/23.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        <#code#>
+    }
+}
