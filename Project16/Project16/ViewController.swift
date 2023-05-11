97dec134b9648530d984cd900f606bcfc68f28a6
Davy <dchuon@ucdavis.edu>
2023-05-11 14:29:04 -0700

    Project 16 challenge 2 completed: created a button that allows users to change map type

--- Project16/Project16/ViewController.swift
+++ Project16/Project16/ViewController.swift
@@ -24,6 +24,49 @@ class ViewController: UIViewController, MKMapViewDelegate {
         let washington = Capital(title: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "Named after George himself.")
   
         mapView.addAnnotations([london, oslo, paris, rome, washington])
+        
+        createMapTypeBtn()
+    }
+    
+    func createMapTypeBtn() {
+        let mapTypeBtn = UIButton(type: .system)
+        mapTypeBtn.frame = CGRect(x: 5, y: 50, width: 150, height: 50)
+        mapTypeBtn.backgroundColor = .gray
+        
+        mapTypeBtn.setTitle("Change view", for: .normal)
+        mapTypeBtn.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        
+        mapTypeBtn.addTarget(self, action: #selector(mapTypeBtnPressed), for: .touchUpInside)
+        mapView.addSubview(mapTypeBtn)
+    }
+    
+    @objc func mapTypeBtnPressed() {
+        let mapTypeAlert = UIAlertController(title: "Select map type", message: nil, preferredStyle: .actionSheet)
+        let mapType1 = UIAlertAction(title: "standard", style: .default) { action in
+            self.mapView.mapType = .standard
+        }
+        let mapType2 = UIAlertAction(title: "satellite", style: .default) { action in
+            self.mapView.mapType = .satellite
+        }
+        let mapType3 = UIAlertAction(title: "hybrid", style: .default) { action in
+            self.mapView.mapType = .hybrid
+        }
+        let mapType4 = UIAlertAction(title: "satellite aerial", style: .default) { action in
+            self.mapView.mapType = .satelliteFlyover
+        }
+        let mapType5 = UIAlertAction(title: "hybrid aerial", style: .default) { action in
+            self.mapView.mapType = .hybridFlyover
+        }
+        let cancel = UIAlertAction(title: "cancel", style: .cancel)
+        
+        mapTypeAlert.addAction(mapType1)
+        mapTypeAlert.addAction(mapType2)
+        mapTypeAlert.addAction(mapType3)
+        mapTypeAlert.addAction(mapType4)
+        mapTypeAlert.addAction(mapType5)
+        mapTypeAlert.addAction(cancel)
+        
+        present(mapTypeAlert, animated: true)
     }
 
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
@@ -43,6 +86,7 @@ class ViewController: UIViewController, MKMapViewDelegate {
             annotationView?.annotation = annotation
         }
         
+        // CHALLENGE 1:
         annotationView?.markerTintColor = .green
         annotationView?.tintColor = .purple
         
@@ -59,6 +103,5 @@ class ViewController: UIViewController, MKMapViewDelegate {
         ac.addAction(UIAlertAction(title: "OK", style: .default))
         present(ac, animated: true)
     }
-
 }
 
