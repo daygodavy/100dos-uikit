fc2a46566ea5faff8c6cc5f82648ff984048f3e7
Davy <dchuon@ucdavis.edu>
2023-05-17 11:43:14 -0700

    Starting Project 20: Fireworks Night

---
+++ Project20/Project20/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  Project20
+//
+//  Created by Davy Chuon on 5/17/23.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+}
