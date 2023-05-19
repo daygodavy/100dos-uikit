d2a9ea5e07d158f00bdcca756d130a24b12b9e32
Davy <dchuon@ucdavis.edu>
2023-05-19 15:37:26 -0700

    Starting Project 22 - Detect a Beacon: set up project and requesting user for location permissions

---
+++ Project22/Project22/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Project22
+//
+//  Created by Davy Chuon on 5/19/23.
+//
+
+import CoreLocation
+import UIKit
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet var distanceReading: UILabel!
+    var locationManager: CLLocationManager?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestAlwaysAuthorization()
+        
+        view.backgroundColor = .gray
+    }
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    // do stuff
+                }
+            }
+        }
+    }
+
+}
+
