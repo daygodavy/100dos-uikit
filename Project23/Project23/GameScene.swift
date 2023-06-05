8c69dd1da543438f35a0ccb88716d7da1f6d6d46
Davy <dchuon@ucdavis.edu>
2023-06-05 12:55:52 -0700

    Starting Project 23: set up for Swift Ninjas and uploaded assets

---
+++ Project23/Project23/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  Project23
+//
+//  Created by Davy Chuon on 5/19/23.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+}
