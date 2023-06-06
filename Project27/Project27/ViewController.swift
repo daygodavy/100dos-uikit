b70ad8b6cc4b75402b123e787334254aee436b95
Davy <dchuon@ucdavis.edu>
2023-06-06 12:09:36 -0700

    Starting Project 27 - Core Graphics: rectangle now rendered on screen

---
+++ Project27/Project27/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  Project27
+//
+//  Created by Davy Chuon on 6/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var currentDrawType = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        drawRectangle()
+    }
+
+    @IBAction func redrawTapped(_ sender: Any) {
+        currentDrawType += 1
+        
+        if currentDrawType > 5 {
+            currentDrawType = 0
+        }
+        
+        switch currentDrawType {
+        case 0:
+            drawRectangle()
+        default:
+            break
+        }
+        
+    }
+    
+    func drawRectangle() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let img = renderer.image { ctx in
+            // drawing code
+            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512)
+            
+            ctx.cgContext.setFillColor(UIColor.red.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.setLineWidth(10)
+            
+            ctx.cgContext.addRect(rectangle)
+            ctx.cgContext.drawPath(using: .fillStroke)
+        }
+        
+        imageView.image = img
+    }
+    
+}
+
