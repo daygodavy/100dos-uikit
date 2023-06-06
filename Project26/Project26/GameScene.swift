5411946e911a40299c09f343a8075b5b76a5b5de
Davy <dchuon@ucdavis.edu>
2023-06-05 17:51:33 -0700

    Starting Project 26 - Marble Maze: added in assets and refactored to have clean set up for code

---
+++ Project26/Project26/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  Project26
+//
+//  Created by Davy Chuon on 6/5/23.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+}
