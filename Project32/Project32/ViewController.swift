e3bd363f137ad1b3e2dd0b410afd69d736693496
Davy <dchuon@ucdavis.edu>
2023-06-07 16:17:33 -0700

    favorites saved to userdefaults, cells now have add/remove favorite feature, favorites indexed/deindexed from core spotlight

--- Project32/Project32/ViewController.swift
+++ Project32/Project32/ViewController.swift
@@ -5,23 +5,29 @@
 //  Created by Davy Chuon on 6/7/23.
 //
 
+import CoreSpotlight
+import MobileCoreServices
 import SafariServices
 import UIKit
 
 class ViewController: UITableViewController {
     var projects = [[String]]()
+    var favorites = [Int]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        projects.append(["Project 1: Storm Viewer", "Constants and variables, UITableView, UIImageView, FileManager, storyboards"])
-        projects.append(["Project 2: Guess the Flag", "@2x and @3x images, asset catalogs, integers, doubles, floats, operators (+= and -=), UIButton, enums, CALayer, UIColor, random numbers, actions, string interpolation, UIAlertController"])
-        projects.append(["Project 3: Social Media", "UIBarButtonItem, UIActivityViewController, the Social framework, URL"])
-        projects.append(["Project 4: Easy Browser", "loadView(), WKWebView, delegation, classes and structs, URLRequest, UIToolbar, UIProgressView., key-value observing"])
-        projects.append(["Project 5: Word Scramble", "Closures, method return values, booleans, NSRange"])
-        projects.append(["Project 6: Auto Layout", "Get to grips with Auto Layout using practical examples and code"])
-        projects.append(["Project 7: Whitehouse Petitions", "JSON, Data, UITabBarController"])
-        projects.append(["Project 8: 7 Swifty Words", "addTarget(), enumerated(), count, index(of:), property observers, range operators."])
+        populateProjects()
+        
+        // retrieve favorites from userdefaults if they exist
+        let defaults = UserDefaults.standard
+        if let savedFavorites = defaults.object(forKey: "favorites") as? [Int] {
+            favorites = savedFavorites
+        }
+        
+        tableView.isEditing = true
+        tableView.allowsSelectionDuringEditing = true
+        
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -34,11 +40,53 @@ class ViewController: UITableViewController {
         let project = projects[indexPath.row]
         cell.textLabel?.attributedText = makeAttributedString(title: project[0], subtitle: project[1])
         
+        if favorites.contains(indexPath.row) {
+            cell.editingAccessoryType = .checkmark
+        } else {
+            cell.editingAccessoryType = .none
+        }
+        
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         showTutorial(indexPath.row)
+    }
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        if favorites.contains(indexPath.row) {
+            return .delete
+        } else {
+            return .insert
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .insert {
+            favorites.append(indexPath.row)
+            index(item: indexPath.row)
+        } else {
+            if let index = favorites.firstIndex(of: indexPath.row) {
+                favorites.remove(at: index)
+                deIndex(item: indexPath.row)
+            }
+        }
+        
+        let defaults = UserDefaults.standard
+        defaults.set(favorites, forKey: "favorites")
+        
+        tableView.reloadRows(at: [indexPath], with: .none)
+    }
+    
+    func populateProjects() {
+        projects.append(["Project 1: Storm Viewer", "Constants and variables, UITableView, UIImageView, FileManager, storyboards"])
+        projects.append(["Project 2: Guess the Flag", "@2x and @3x images, asset catalogs, integers, doubles, floats, operators (+= and -=), UIButton, enums, CALayer, UIColor, random numbers, actions, string interpolation, UIAlertController"])
+        projects.append(["Project 3: Social Media", "UIBarButtonItem, UIActivityViewController, the Social framework, URL"])
+        projects.append(["Project 4: Easy Browser", "loadView(), WKWebView, delegation, classes and structs, URLRequest, UIToolbar, UIProgressView., key-value observing"])
+        projects.append(["Project 5: Word Scramble", "Closures, method return values, booleans, NSRange"])
+        projects.append(["Project 6: Auto Layout", "Get to grips with Auto Layout using practical examples and code"])
+        projects.append(["Project 7: Whitehouse Petitions", "JSON, Data, UITabBarController"])
+        projects.append(["Project 8: 7 Swifty Words", "addTarget(), enumerated(), count, index(of:), property observers, range operators."])
     }
     
     func makeAttributedString(title: String, subtitle: String) -> NSAttributedString {
@@ -62,6 +110,40 @@ class ViewController: UITableViewController {
             let vc = SFSafariViewController(url: url, configuration: config)
             present(vc, animated: true)
         }
+    }
+    
+    func index(item: Int) {
+        let project = projects[item]
+        
+        // set the attributes for the item to be indexed
+        let attributeSet = CSSearchableItemAttributeSet(itemContentType: kUTTypeText as String)
+        attributeSet.title = project[0]
+        attributeSet.contentDescription = project[1]
+        
+        // create the searchable item with ID and attributes
+        let item = CSSearchableItem(uniqueIdentifier: "\(item)", domainIdentifier: "com.hackingwithswift", attributeSet: attributeSet)
+        // index expires one month after being added by default; set so they don't expire
+        item.expirationDate = Date.distantFuture
+        
+        // index the item (method runs async so use trailing closure to determine success or not)
+        CSSearchableIndex.default().indexSearchableItems([item]) { error in
+            if let error = error {
+                print("Indexing error: \(error.localizedDescription)")
+            } else {
+                print("Search item successfully indexed!")
+            }
+        }
+    }
+    
+    func deIndex(item: Int) {
+        CSSearchableIndex.default().deleteSearchableItems(withIdentifiers: ["\(item)"]) { error in
+            if let error = error {
+                print("Deindexing error: \(error.localizedDescription)")
+            } else {
+                print("Search item successfully removed!")
+            }
+        }
+        
     }
     
 
