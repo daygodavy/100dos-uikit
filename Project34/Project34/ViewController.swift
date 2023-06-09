bb74b90433b9a2b5c484f63f13996ea1b1ce44f5
Davy <dchuon@ucdavis.edu>
2023-06-09 11:52:31 -0700

    Starting Project 34 - Four in a Row: setup stackview with 7 buttons

---
+++ Project34/Project34/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Project34
+//
+//  Created by Davy Chuon on 6/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var columnButtons: [UIButton]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func makeMove(_ sender: UIButton) {
+    }
+    
+}
+
