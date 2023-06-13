d252d1bcdf950754ff2a7d826e2b32012b802a30
Davy <dchuon@ucdavis.edu>
2023-06-13 14:52:01 -0700

    Starting Project 38 - GitHub Commits

---
+++ Project38/Project38/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project38
+//
+//  Created by Davy Chuon on 6/13/23.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
