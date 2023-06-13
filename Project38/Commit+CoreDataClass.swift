0017c1cfd9892a3aec46e78bdeef448f43ff20a8
Davy <dchuon@ucdavis.edu>
2023-06-13 15:54:52 -0700

    added functions for fetching commits and configuring the data

---
+++ Project38/Commit+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Commit+CoreDataClass.swift
+//  Project38
+//
+//  Created by Davy Chuon on 6/13/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Commit)
+public class Commit: NSManagedObject {
+
+}
