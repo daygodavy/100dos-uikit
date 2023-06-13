0ef6d69885d5b0b44f285f94d9531cc6563092a5
Davy <dchuon@ucdavis.edu>
2023-06-13 15:23:58 -0700

    created persistent container for data model and implemented function to save to the db

--- Project38/Project38/ViewController.swift
+++ Project38/Project38/ViewController.swift
@@ -5,13 +5,34 @@
 //  Created by Davy Chuon on 6/13/23.
 //
 
+import CoreData
 import UIKit
 
 class ViewController: UITableViewController {
+    var container: NSPersistentContainer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // creates the persistent container, and must be given the name of the Core Data model file we created
+        container = NSPersistentContainer(name: "Project38")
+        
+        // loads the saved database if it exists, or creates it otherwise
+        container.loadPersistentStores { storeDescription, error in
+            if let error = error {
+                print("Unresolved error: \(error)")
+            }
+        }
+    }
+    
+    func saveContext() {
+        if container.viewContext.hasChanges {
+            do {
+                try container.viewContext.save()
+            } catch {
+                print("An error occurred while saving: \(error)")
+            }
+        }
     }
 
 
