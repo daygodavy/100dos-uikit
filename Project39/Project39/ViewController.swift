435a4eba2db91ce5b068cc5bdcda03897e482951
Davy <dchuon@ucdavis.edu>
2023-06-14 12:23:24 -0700

    Starting Projet 39 - XCTest Unit Testing

---
+++ Project39/Project39/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project39
+//
+//  Created by Davy Chuon on 6/14/23.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
